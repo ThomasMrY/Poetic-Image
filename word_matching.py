895ce24a110cd6ffa04757c6f059361c3769705e
Xiaoqiang Zhou <13436433445@126.com>
2018-11-27 20:55:28 +0800

    Update word_matching.py

--- word_matching.py
+++ word_matching.py
@@ -4,6 +4,7 @@ from functools import partial
 import sys
 import json
 import requests
+import synonyms
 
 def translate(word):
     # 有道词典 api
@@ -14,6 +15,8 @@ def translate(word):
         'i': word,
         "doctype": "json",
         "version": "2.1",
+        #'from': 'AUTO',
+        #'to': 'Chinese',
         "keyfrom": "fanyi.web",
         "ue": "UTF-8",
         "action": "FY_BY_CLICKBUTTON",
@@ -23,36 +26,36 @@ def translate(word):
     response = requests.post(url, data=key)
     # 判断服务器是否相应成功
     if response.status_code == 200:
-        # 然后相应的结果
         return response.text
     else:
-        print("有道词典调用失败")
-        # 相应失败就返回空
+        #print("有道词典调用失败")
         return None
 
 def get_reuslt(repsonse):
-    # 通过 json.loads 把返回的结果加载成 json 格式
     result = json.loads(repsonse)
-    print ("输入的词为：%s" % result['translateResult'][0][0]['src'])
-    print ("翻译结果为：%s" % result['translateResult'][0][0]['tgt'])
-
+    return result['translateResult'][0][0]['tgt']
 
 def main():
+    start = time.clock()
     filename = './couplet_100k.txt'
-    input_tag_list = ['酒','月','水','风','山','云','一','眼']
-    retrieval_result_length = 9
+    #input_tag_list = ['chair', 'moon','mountain']
+    input_tag_list = ['moon']
+    list_trans = []
+    final_input_tag_list = []
+    for i in range(len(input_tag_list)):
+        list_trans.append(get_reuslt(translate(input_tag_list[i])))
+    
+    for i in range(len(list_trans)):
+        synonyms_words = synonyms.nearby(list_trans[i])
+        d=dict(zip(synonyms_words[0],synonyms_words[1]))
+        synonyms_words = [k for k,v in d.items() if v >=0.7]
+        final_input_tag_list += synonyms_words[:3]
+    
     final_result_length = 5
+    retrieval_results = []
+    
     with open(filename, 'r', encoding='utf-8') as in_file:
         all_lines = in_file.readlines()
-    retrieval_results = []
-    start = time.clock()
-    #'''
-    for i in range(len(input_tag_list)):
-        tag = input_tag_list[i]
-        #tag_retrieval_result = retrieve_tag(all_lines,result_length = retrieval_result_length, tag )
-        tag_retrieval_result = retrieve_tag(all_lines,-1, tag)
-        #retrieval_results.append(tag_retrieval_result)
-        retrieval_results+=tag_retrieval_result
     ''' # much slower?
     pool = ThreadPool()
     func = partial(retrieve_tag, all_lines, -1)
@@ -60,20 +63,21 @@ def main():
     pool.close()
     pool.join()
     '''
+    for i in range(len(final_input_tag_list)):
+        tag = final_input_tag_list[i]
+        #tag_retrieval_result = retrieve_tag(all_lines,result_length = retrieval_result_length, tag )
+        tag_retrieval_result = retrieve_tag(all_lines,-1, tag)
+        retrieval_results+=tag_retrieval_result
+    #'''
     results = {}
     for i in retrieval_results:
         results[i] = results.get(i, 0) + 1 
-        #results = sorted(results.items(), key=lambda item:item[0]) 
     results = sorted(results.items(), key=lambda item: item[1], reverse=True)
-    tic = time.clock()-start
-    print(tic)
-    print("本程序调用有道词典的API进行翻译，可达到以下效果：")
-    print("外文-->中文")
-    print("中文-->英文")
-    word = input('请输入你想要翻译的词或句：')
-    list_trans = translate(word)
-    get_reuslt(list_trans)
-
+    output_results_index = [index[0] for index in results[:5]]
+    results = [all_lines[i][:-1] for i in output_results_index] # ['一树桂花为月亮', '一鉴荒唐月亮猪', '云追月亮风吹柳', '今夜烟花同月亮', '今宵月亮真圆啊']
+    #tic = time.clock()-start
+    #print(tic)
+    return results
 
 def retrieve_tag(content, result_length, tag):
     tag_retrieval_index = []
@@ -86,4 +90,5 @@ def retrieve_tag(content, result_length, tag):
     return tag_retrieval_index
 
 if __name__ == "__main__":
-    main()
+    results = main()
+    print(results)

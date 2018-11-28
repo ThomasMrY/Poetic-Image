a497679b601fae21239d29049c536dcb29d96431
Xiaoqiang Zhou <13436433445@126.com>
2018-11-28 20:34:12 +0800

    Update word_matching.py

--- word_matching.py
+++ word_matching.py
@@ -5,6 +5,7 @@ import sys
 import json
 import requests
 import synonyms
+from RS import call_cv_api
 
 def translate(word):
     # 有道词典 api
@@ -35,25 +36,31 @@ def get_reuslt(repsonse):
     result = json.loads(repsonse)
     return result['translateResult'][0][0]['tgt']
 
-def main():
+def find_shanglian(input_info, tag_mode=1):
     start = time.clock()
     filename = './couplet_100k.txt'
     #input_tag_list = ['chair', 'moon','mountain']
     input_tag_list = ['moon']
+    final_result_length = 5
+    input_tag_list = input_info['description']['tags']
+    print(input_tag_list)
     list_trans = []
     final_input_tag_list = []
     for i in range(len(input_tag_list)):
         list_trans.append(get_reuslt(translate(input_tag_list[i])))
-    
+    print(list_trans)
+    list_trans = list(set(list_trans))
+    print(list_trans)
     for i in range(len(list_trans)):
         synonyms_words = synonyms.nearby(list_trans[i])
         d=dict(zip(synonyms_words[0],synonyms_words[1]))
         synonyms_words = [k for k,v in d.items() if v >=0.7]
-        final_input_tag_list += synonyms_words[:3]
-    
-    final_result_length = 5
+        if tag_mode==1:
+            final_input_tag_list += synonyms_words[:3]
+        else:
+            final_input_tag_list.append(synonyms_words[:3])
     retrieval_results = []
-    
+    print(final_input_tag_list)
     with open(filename, 'r', encoding='utf-8') as in_file:
         all_lines = in_file.readlines()
     ''' # much slower?
@@ -64,31 +71,51 @@ def main():
     pool.join()
     '''
     for i in range(len(final_input_tag_list)):
-        tag = final_input_tag_list[i]
-        #tag_retrieval_result = retrieve_tag(all_lines,result_length = retrieval_result_length, tag )
-        tag_retrieval_result = retrieve_tag(all_lines,-1, tag)
-        retrieval_results+=tag_retrieval_result
+        if tag_mode==1:
+            tag = final_input_tag_list[i]
+            #tag_retrieval_result = retrieve_tag(all_lines,result_length = retrieval_result_length, tag )
+            tag_retrieval_result = retrieve_tag(all_lines,-1, tag)
+            retrieval_results+=tag_retrieval_result
+        else:
+            tag = final_input_tag_list[i]
+            tag_retrieval_result = retrieve_tag(all_lines,-1, tag, tag_mode=2)
+            retrieval_results+=tag_retrieval_result
     #'''
     results = {}
     for i in retrieval_results:
         results[i] = results.get(i, 0) + 1 
     results = sorted(results.items(), key=lambda item: item[1], reverse=True)
-    output_results_index = [index[0] for index in results[:5]]
-    results = [all_lines[i][:-1] for i in output_results_index] # ['一树桂花为月亮', '一鉴荒唐月亮猪', '云追月亮风吹柳', '今夜烟花同月亮', '今宵月亮真圆啊']
+    output_results_index = [index[0] for index in results[:final_result_length]]
+    print ([index[1] for index in results[:final_result_length]])
+    results = [all_lines[i][:-1] for i in output_results_index]
     #tic = time.clock()-start
     #print(tic)
     return results
 
-def retrieve_tag(content, result_length, tag):
+def retrieve_tag(content, result_length, tag, tag_mode=1):
     tag_retrieval_index = []
-    for j in range(int(len(content)/3)):
-        data_sentence = content[j*3]
-        if (data_sentence.find(tag))!= -1:
-            tag_retrieval_index.append(j*3)
-        if len(tag_retrieval_index)==result_length:
-            break
-    return tag_retrieval_index
+    if tag_mode==1:
+        for j in range(int(len(content)/3)):
+            data_sentence = content[j*3]
+            if (data_sentence.find(tag))!= -1:
+                tag_retrieval_index.append(j*3)
+            if len(tag_retrieval_index)==result_length:
+                break
+        return tag_retrieval_index
+    elif tag_mode==2:
+        for i in range(len(tag)):
+            result_index = []
+            for j in range(int(len(content)/3)):
+                data_sentence = content[j*3]
+                if (data_sentence.find(tag[i]))!= -1:
+                    #result_index.append(j*3)
+                    tag_retrieval_index.append(j*3)
+                if len(tag_retrieval_index)==result_length:
+                    break
+        return tag_retrieval_index
 
 if __name__ == "__main__":
-    results = main()
-    print(results)
+    with open('./download1.jpg', 'rb') as f:
+        input_info = call_cv_api(f.read())
+        results = find_shanglian(input_info)
+        print(results)

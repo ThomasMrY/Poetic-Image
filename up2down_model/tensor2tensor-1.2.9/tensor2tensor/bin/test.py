c8cb7c07106f4aac48459a6f9f851862ae629f65
YichongLeng <lyc123go@163.com>
2018-12-05 20:06:59 +0800

    Create test.py

---
+++ up2down_model/tensor2tensor-1.2.9/tensor2tensor/bin/test.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+import up2down_class as uc
+a = uc.up2down_class()
+print(a.get_next("海 阔 天 空"))
+print(a.get_next("亲 亲 我"))
+print(a.get_next("喜 欢 科 大"))
+print(a.get_next("微 软 公 司"))
